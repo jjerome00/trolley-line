142a5ff0d3ddec1bc5a6d3123651c648470eb773
Jason Jerome <jason@jjerome.com>
2024-10-10 14:56:51 -0700

    Update docs with more information Fix bug: if/else-if for sensor checking Update docs/logging Separate handling of sensor tripped and time expiration

--- physical configuration with wires.md
+++ physical configuration with wires.md
@@ -1,25 +1,47 @@
 
 
-Music
-Board:Ground -> brown-wire > arduino Ground
-Board:VIN > red-wire > arduino VIN 
-Board:0pin (music) > orange-wire > arudino pin13
+---
+## Music
+Music requires power, a "button" pin, and ground (pin + ground activate the button)
+Power is supplied via usb connection - easiest way since it can get pure 5v
 
-Motor Controller
-Board:5v > orange-wire > arudino 5v
-Board:Gnd > purple-wire > arudino Gnd
-Board:SCL > green-wire > arudino SCL
-Board:SDA > yellow-wire > arudino SDA
+The Music board connects its 0pin to arduino pin 12 
+The Music board ground connects to arduino ground
 
-Senors
-note: sensors share their 5v + Gnd
-The 5v + Gnd + Left (signal) + Right (signal) are all fed into a single 4-block terminal
+note: I mistakenly used pin 13 on arduino in the past - big mistake 
+That's the pin arduino uses for its own internal led mounted on the board for testing
+
+Music Board:Ground -> brown-wire > arduino Ground
+Music Board:0pin (music) > orange-wire > arudino pin12
+Music Board usb-connection: 5V power supply (old phone charger)
+
+Speakers use separate USB power and plug into music board via 3.5mm jack
+
+---
+## Motor Controller
+Motor Board:5v > orange-wire > arudino 5v
+Motor Board:Gnd > purple-wire > arudino Gnd
+Motor Board:SCL > green-wire > arudino SCL
+Motor Board:SDA > yellow-wire > arudino SDA
+
+---
+## Senors
+9/2024: I switched the break-beam sensors for a limit switch. 
+The same logic works except the limit switch does not need power. 
+I only use the ground + pin
 
 Right side (or Pin One in code)
-Arduino pin4 > yellow-wire > [block terminal] > White-wire for Right side (in bundle)
+Arduino pin4 > yellow-wire > White-wire for Right side (in bundle)
 
 Left side (or Pin Two in code)
-Arduino pin2 > white-wire > [block terminal] > White-wire for Left side (in bundle)
+Arduino pin2 > white-wire > White-wire for Left side (in bundle)
 
 Arduino 5v > red wire > [block terminal] > to the senors + button (you must share!)
 Arudino Gnd > black (via marker) wire > [block terminal] > to the sensors + button (you must share!)
+
+---
+## Button
+Arduino 7: button pin (starts trolley)
+Arduino 8: button LED 
+Arduino Ground
+
--- trolley-line/trolley-line.ino
+++ trolley-line/trolley-line.ino
@@ -5,6 +5,11 @@
  * Motor control to move the trolley (plus direction)
  * Button to start
  * 
+ * Left and Right are positions as if you are 
+ * standing in front of the display
+ * 
+ * FORWARD is going towards the Left side
+ * BACKWARD is going towards the Right side
  */
 
 // Motor controller
@@ -19,18 +24,18 @@ Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
 // (End) Motor controller
 
 #define LEDPIN 13
-#define SENSOR_PIN_ONE 4  //right
-#define SENSOR_PIN_TWO 2  //left
+#define SENSOR_PIN_RIGHT 4 // right (ONE)
+#define SENSOR_PIN_LEFT 2  // left (TWO)
 #define BUTTON_PIN 7
 #define BUTTON_PIN_LED 8
 #define MUSIC_PIN 12
 
 // current set direction of the motor (forward or reverse)
-int direction = FORWARD;
+int direction = FORWARD; //forward is towards Left
 
-// current & last state of both ir sensors (one & two)
-int irSensorOne = 0, irOneLastState = 0;
-int irSensorTwo = 0, irTwoLastState = 0;
+// current & last state of both ir sensors (left & right)
+int irSensorRight = 0, irSensorRightLast = 0;
+int irSensorLeft = 0, irSensorLeftLast = 0;
 
 // current state of the trolley/motor, moving or not
 bool moving = false;
@@ -38,8 +43,9 @@ bool moving = false;
 // used for detecting idle state while debugging (see loop())
 bool idleLogFlag = false;
 
-// time limit: adjust to your needs, it takes current trolley about 22 seconds to make it between sensors
-unsigned long total_time = 23000;
+// time limit: just in case a sensor doesn't work 
+// adjust to your needs, it takes current trolley <20 seconds to make it between sensors
+unsigned long total_time = 20000;
 unsigned long current_time = 0;
 unsigned long previous_time = 0;
 
@@ -49,22 +55,23 @@ void setup() {
   pinMode(LEDPIN, OUTPUT); // ready pin (on arudino board - debug only)
   pinMode(BUTTON_PIN, INPUT_PULLUP);  // start button
   pinMode(BUTTON_PIN_LED, OUTPUT); // start button's LED
-  pinMode(SENSOR_PIN_ONE, INPUT); // sensor one
-  pinMode(SENSOR_PIN_TWO, INPUT); // sensor two
+  pinMode(SENSOR_PIN_RIGHT, INPUT); // sensor one / right
+  pinMode(SENSOR_PIN_LEFT, INPUT); // sensor two / left
   pinMode(MUSIC_PIN, OUTPUT); // music
 
   digitalWrite(LEDPIN, HIGH);
   digitalWrite(BUTTON_PIN, HIGH);
   digitalWrite(BUTTON_PIN_LED, HIGH);
-  digitalWrite(SENSOR_PIN_ONE, HIGH);
-  digitalWrite(SENSOR_PIN_TWO, HIGH);
+  digitalWrite(SENSOR_PIN_RIGHT, HIGH);
+  digitalWrite(SENSOR_PIN_LEFT, HIGH);
+  
   playMusic(false);
 
   Serial.println("Hello!");
   
   // setup motor
   AFMS.begin();
-  myMotor->setSpeed(175);
+  myMotor->setSpeed(155);
 
   Serial.println("Ready!");
   Serial.println("Starting trolley on first run");
@@ -78,7 +85,6 @@ void loop() {
 
     if (previous_time == 0) {
       // if previous_time is set to zero, set it to current_time
-      // (this is a guard - just in case someone forgot to set previous_time)
       previous_time = current_time;
     }
     
@@ -87,16 +93,16 @@ void loop() {
 
     // only check the sensor that the trolley is moving towards
     if (direction == FORWARD) {
-      checkSensor(2, SENSOR_PIN_TWO, irSensorTwo, irTwoLastState, BACKWARD);
+      checkSensor(2, SENSOR_PIN_LEFT, irSensorLeft, irSensorLeftLast, BACKWARD);
+    }
+    else if (direction == BACKWARD) {
+      checkSensor(1, SENSOR_PIN_RIGHT, irSensorRight, irSensorRightLast, FORWARD);
     }
 
-    if (direction == BACKWARD) {
-      checkSensor(1, SENSOR_PIN_ONE, irSensorOne, irOneLastState, FORWARD);
-    }
-
+    // check to see if we've stopped and update state
     if (!moving) {
       playMusic(false);
-      // if we have stopped, pause before allowing another button press
+      // pause before allowing another button press
       delay(4000);
       setReadyLight(true);
     }
@@ -104,15 +110,18 @@ void loop() {
   } else {
     // we're not moving - wait for button press
 
+    // ensure we're not playing music
+    playMusic(false);
+
     if (!idleLogFlag) {
-      // this is used so that we only log idling once (and not a million times)
+      // ensure we only log "idling" once (and not a million times)
       Serial.println("Idling...");
       idleLogFlag = true;  
     }
 
     if (digitalRead(BUTTON_PIN) == LOW) {
       // start button has been pressed 
-      // - the other side of this if statement will be run when the loop continues
+      // - the other side of main if statement will be run when the loop continues
       startTrolley();
     }
   }
@@ -122,14 +131,24 @@ void loop() {
  * set the `moving` flag so that the trolley will start in the next iteration of loop()
  */
 void startTrolley() {
-  Serial.println("button pressed - STARTING!");
+  if (direction == FORWARD) {
+    Serial.println("button pressed - START going LEFT");
+  } else {
+    Serial.println("button pressed - START going RIGHT");
+  }
+  
   resetIRState();
   moving = true;
   setReadyLight(false);
   playMusic(true);
-
-  // start counting time
+  
+  // idleLogFlag is used for debugging - see loop()
+  idleLogFlag = false;
+  
+  // start counting time; used to determine how long it's been moving
   previous_time = current_time;
+
+  // direction is already set, motor will start when it hits the main loop() function
 }
 
 void playMusic(bool play) {
@@ -142,12 +161,10 @@ void playMusic(bool play) {
 
 /**
  * reset all the "last state" variables
- * idleLogFlag is used for debugging - see loop()
  */
 void resetIRState() {
-  irOneLastState = 1;
-  irTwoLastState = 1;
-  idleLogFlag = false;
+  irSensorRightLast = 1;
+  irSensorLeftLast = 1;
 }
 
 /**
@@ -168,6 +185,19 @@ void setReadyLight(bool onState) {
 }
 
 /**
+ * stop everything, wait one second
+ * called from checkSensor(...)
+ */
+void stopEverything() {
+  moving = false;
+  playMusic(false);
+  myMotor->run(RELEASE);
+  current_time = 0;
+  previous_time = 0;
+  delay(1000);
+}
+
+/**
  * Check current state of sensor given. If sensor tripped stop the motor & change direction of motor (do not start it)
  * These state variables are passed in by reference, so make sure these are correct.
  */
@@ -179,29 +209,35 @@ void checkSensor(int id, int sensorPin, int &irCurrentState, int &irLastState, i
   bool sensorTripped = (irCurrentState != irLastState);
 
   // check against the last reading of state (this indicates if the sensor has been tripped)
-  if (sensorTripped || timeIsUp) {
+  if (sensorTripped) {
     //stop motor
-      
-    // log result
-    if (sensorTripped) {
-      Serial.print("STOPPED: IR Sensor tripped: ");
-      Serial.println(id);
+    if (id == 2) {
+      Serial.println("STOPPED: IR Sensor tripped on LEFT (2)");
+    } else {
+      Serial.println("STOPPED: IR Sensor tripped on RIGHT (1)");
     }
-    if (timeIsUp) {
-      Serial.println("STOPPED: time is up");
+    
+    // stop everything, wait one second
+    stopEverything();
+
+    // change direction (to be ready for next button press)
+    direction = newDirection;
+  }
+  else if (timeIsUp) {
+    //stop motor - done separately to keep from screwing up the state
+    if (id == 2) {
+      Serial.println("STOPPED: time is up; sensor was LEFT (2)");
+    } else {
+      Serial.println("STOPPED: time is up; sensor was RIGHT (1)");
     }
 
     // stop everything, wait one second
-    moving = false;
-    playMusic(false);
-    myMotor->run(RELEASE);
-    current_time = 0;
-    previous_time = 0;
-    delay(1000);
+    stopEverything();
 
     // change direction (to be ready for next button press)
     direction = newDirection;
   }
 
+  // save current state until the next time we check it
   irLastState = irCurrentState;
 }
